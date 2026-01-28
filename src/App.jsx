4a33969fa31232acea865058f2266cdc9a696cf4
FoysalHossain2 <fh375170@gmail.com>
2026-01-28 22:45:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./component/header/Header"
+
+function App() {
+
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/header/Header.jsx
@@ -0,0 +1,31 @@
+import Favourite from "./Favourite"
+import FavouriteModalList from "./FavouriteModalList"
+import From from "./SearchBar"
+import Logo from "./Logo"
+
+
+const Header = () => {
+  return (
+    <header className="fixed w-full top-0 z-50 bg-linear-to-b from-black/60 to-black/0 pb-10">
+		<nav className="flex items-center justify-between py-6">
+
+            {/* logo */}
+            <Logo />
+
+			<div className="flex items-center gap-4 relative">
+                {/* from */}
+                <From />
+
+                {/* Favourite */}
+                <Favourite/>
+
+				{/* <!-- Modal --> */}
+				<FavouriteModalList />
+
+			</div>
+		</nav>
+	</header>
+  )
+}
+
+export default Header
